360b9111a6b2ffa13fd24a5703a2244075bc1757
Lyle Xu <92287281+luxu-ms@users.noreply.github.com>
2025-12-08 14:15:10 +0800

    Code migration: Migrate Log4j 1.x to Log4j 2.x
    - Updated dependencies in all projects (log4j-v1-project, slf4j-log4j-project, spring-log4j-transitive)
    - Replaced log4j 1.2.17 with log4j-api and log4j-core 2.23.1
    - Replaced slf4j-log4j12 with log4j-slf4j-impl
    - Converted log4j.properties to log4j2.xml format in all projects
    - Updated source code in log4j-v1-project to use Log4j 2.x API
    - Deleted old log4j.properties files
    - Updated spring-log4j-transitive README.md to reflect Log4j 2.x usage

--- log4j-v1-project/src/main/java/com/example/App.java
+++ log4j-v1-project/src/main/java/com/example/App.java
@@ -1,14 +1,12 @@
 package com.example;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class App {
-    private static final Logger logger = Logger.getLogger(App.class);
+    private static final Logger logger = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
-        // Configure Log4j
-        PropertyConfigurator.configure("log4j.properties");
 
         logger.debug("Debug message");
         logger.info("Info message - Application started");
